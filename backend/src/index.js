e931150066fd91b868084b764dd594b3505e012d
Aditya Maurya <aditya081203@gmail.com>
2026-06-30 00:18:52 +0530

    chore: update production backend CORS origin allow-list

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,10 +28,10 @@ const httpServer = createServer(app);
 initializeSocket(httpServer);
 
 app.use(
-	cors({
-		origin: "http://localhost:3000",
-		credentials: true,
-	})
+    cors({
+        origin: ["https://spotiplay-app.vercel.app/", "http://localhost:3000"],
+        credentials: true,
+    })
 );
 
 app.use(express.json()); // to parse req.body
